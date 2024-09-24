d38f1d9adfba30252e81190c73a261094b395858
Blinkfrosty <drahman032@gmail.com>
2024-09-24 00:44:05 -0400

    Added logout button to drawer menu

--- app/src/main/java/com/blinkfrosty/medfinder/MainActivity.java
+++ app/src/main/java/com/blinkfrosty/medfinder/MainActivity.java
@@ -1,10 +1,15 @@
 package com.blinkfrosty.medfinder;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -38,6 +43,21 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_logout) {
+                showLogoutDialog();
+                return true;
+            } else {
+                boolean handled = NavigationUI.onNavDestinationSelected(item, navController)
+                        || MainActivity.super.onOptionsItemSelected(item);
+                if (handled) {
+                    binding.drawerLayout.closeDrawer(GravityCompat.START);
+                }
+                return handled;
+            }
+        });
     }
 
     @Override
@@ -52,5 +72,24 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    private void showLogoutDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View dialogView = getLayoutInflater().inflate(R.layout.dialog_logout_confirmation, null);
+        builder.setView(dialogView);
+
+        AlertDialog dialog = builder.create();
+
+        Button buttonNo = dialogView.findViewById(R.id.button_no);
+        buttonNo.setOnClickListener(v -> dialog.dismiss());
+
+        Button buttonYes = dialogView.findViewById(R.id.button_yes);
+        buttonYes.setOnClickListener(v -> {
+            // TODO: Implement logout and remove this toast message
+            Toast.makeText(MainActivity.this, "Logout not implemented yet", Toast.LENGTH_SHORT).show();
+        });
+
+        dialog.show();
     }
 }
